84212c70fdfc3e466d6fe4a935ee51022f373ae9
Cody House <codyahouse@gmail.com>
2015-01-06 13:35:27 -0500

    added log transitions for debugging purposes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,10 @@
 //= require ./app
 
 // for more details see: http://emberjs.com/guides/application/
-App = Ember.Application.create();
+App = Ember.Application.create({
+  LOG_TRANSITIONS: true,
+  LOG_TRANSITIONS_INTERNAL: true,
+  LOG_VIEW_LOOKUPS: true
+});
 
 //= require_tree .
