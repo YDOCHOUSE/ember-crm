d0f0bddbca1ca4f835a3aeac7f039aa9e0cb606c
Cody House <codyahouse@gmail.com>
2015-01-06 16:13:04 -0500

    added messages to let users know if their contacts have been edited but not saved

--- app/assets/javascripts/controllers/lead.js
+++ app/assets/javascripts/controllers/lead.js
@@ -1,5 +1,9 @@
 App.LeadController = Ember.ObjectController.extend({
-
+  
+  showUnsavedMessage: function() {
+    return this.get('isDirty') && !this.get('isSaving')
+  }.property('isDirty', 'isSaving'),
+  
   actions: {
     saveChanges: function() {
       if (this.get('model.isDirty')) 
